d7e8a502d2e00ce24b2f65658b2c672595f9984f
angelo <70670914+operator-19@users.noreply.github.com>
2022-11-04 00:06:19 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal') // select all
+console.log(btnsOpenModal);
+
+const closeModal = function () {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+const openModal = function () {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', openModal); // do something for each of btns
+
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) { // esc feature
+        // if (!modal.classList.contains('hidden')){
+        //     closeModal();
+        // }
+        closeModal();
+    }
+})
